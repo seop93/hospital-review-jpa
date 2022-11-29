5f4c8bbd27f543ec988663acab50608d4f50bf6e
byeonheungseob <kevin9160@naver.com>
2022-11-29 15:46:39 +0900

    수정

---
+++ src/main/java/com/hospital/review/domain/UserLoginResponse.java
@@ -0,0 +1,10 @@
+package com.hospital.review.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UserLoginResponse {
+    private String token;
+}
